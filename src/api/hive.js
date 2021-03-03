4a4f21fe2eb504922df559a253cecac4e502f16e
Benny Blockchain <ben@styng.social>
2021-03-03 00:37:35 -0500

    patch hive api

--- src/api/hive.js
+++ src/api/hive.js
@@ -9,8 +9,8 @@ export const getDiscussion = async (author, permlink) => {
   return discussion;
 };
 
-export const getTrending = async (tag = "") => {
-  const params = { limit: 20, tag: tag, category: "hive-167922" };
+export const getTrending = async (tag) => {
+  const params = { limit: 20, tag: tag };
   const trending = await hive.api.callAsync(
     "condenser_api.get_discussions_by_trending",
     params
